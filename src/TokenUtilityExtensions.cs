5e37d47ecafeff7615029264f5e27ac65d179d36
Dominick Baier <dbaier@leastprivilege.com>
2019-05-22 16:22:34 +0100

    cleanup

--- src/TokenUtilityExtensions.cs
+++ src/TokenUtilityExtensions.cs
@@ -2,49 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using System;
 using System.Threading.Tasks;
 
 namespace IdentityModel.AspNetCore
 {
     public static class TokenUtilityExtensions
     {
-        public static async Task<(string accessToken, string refreshToken)> RefreshAccessTokenAsync(this HttpContext context, string refreshToken)
-        {
-            var service = context.RequestServices.GetRequiredService<TokenEndpointService>();
-            var result = await service.RefreshTokenAsync(refreshToken);
-
-            if (!result.IsError)
-            {
-                return (result.AccessToken, result.RefreshToken);
-            }
-
-            return (null, null);
-        }
-
-        public static async Task<(string accessToken, string refreshToken)> RefreshAccessTokenAsync(this HttpContext context)
-        {
-            var service = context.RequestServices.GetRequiredService<TokenEndpointService>();
-            var store = context.RequestServices.GetRequiredService<ITokenStore>();
-
-            var tokens = await store.GetTokenAsync(context.User);
-            var result = await service.RefreshTokenAsync(tokens.refreshToken);
-
-            if (!result.IsError)
-            {
-                await store.StoreTokenAsync(context.User, result.AccessToken, result.ExpiresIn, result.RefreshToken);
-                return (result.AccessToken, result.RefreshToken);
-            }
-
-            return (null, null);
-        }
-
-        public static async Task RevokeRefreshTokenAsync(this HttpContext context, string refreshToken)
-        {
-            var service = context.RequestServices.GetRequiredService<TokenEndpointService>();
-            await service.RevokeTokenAsync(refreshToken);
-        }
-
         public static async Task<string> GetAccessTokenAsync(this HttpContext context)
         {
             var store = context.RequestServices.GetRequiredService<ITokenStore>();
@@ -61,6 +24,36 @@ namespace IdentityModel.AspNetCore
             }
 
             return tokens.accessToken;
+        }
+
+        public static async Task<(string accessToken, int expiresIn, string refreshToken)> RefreshAccessTokenAsync(this HttpContext context, string refreshToken)
+        {
+            var service = context.RequestServices.GetRequiredService<TokenEndpointService>();
+            var result = await service.RefreshTokenAsync(refreshToken);
+
+            if (!result.IsError)
+            {
+                return (result.AccessToken, result.ExpiresIn, result.RefreshToken);
+            }
+
+            throw new System.Exception(result.Error);
+        }
+
+        public static async Task<(string accessToken, string refreshToken)> RefreshAccessTokenAsync(this HttpContext context)
+        {
+            var store = context.RequestServices.GetRequiredService<ITokenStore>();
+
+            var tokens = await store.GetTokenAsync(context.User);
+            var result = await context.RefreshAccessTokenAsync(tokens.refreshToken);
+
+            await store.StoreTokenAsync(context.User, result.accessToken, result.expiresIn, result.refreshToken);
+            return (result.accessToken, result.refreshToken);
+        }
+
+        public static async Task RevokeRefreshTokenAsync(this HttpContext context, string refreshToken)
+        {
+            var service = context.RequestServices.GetRequiredService<TokenEndpointService>();
+            await service.RevokeTokenAsync(refreshToken);
         }
     }
 }
