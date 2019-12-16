38e5ae1fbd0a7d2264d97723c8b19591452cb738
Dominick Baier <dbaier@leastprivilege.com>
2019-12-16 10:22:58 +0100

    logging cleanup

--- samples/TokenManagement3/Program.cs
+++ samples/TokenManagement3/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace MvcCode
 {
@@ -9,24 +10,24 @@ namespace MvcCode
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("System", LogEventLevel.Error)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                .MinimumLevel.Override("System.Net.Http", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseSerilog((context, config) =>
-                    {
-                         config
-                             .MinimumLevel.Warning()
-                             .MinimumLevel.Override("IdentityModel", LogEventLevel.Debug)
-                             .MinimumLevel.Override("System.Net.Http", LogEventLevel.Information)
-                             .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                             .Enrich.FromLogContext()
-                             .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}");
-                    });
                 });
     }
 }
