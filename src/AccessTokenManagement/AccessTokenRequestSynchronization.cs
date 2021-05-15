2d9460a110991c35eac09c3925c21ef8e4b010a2
Dominick Baier <dbaier@leastprivilege.com>
2021-05-15 09:38:20 +0200

    add comments

--- src/AccessTokenManagement/AccessTokenRequestSynchronization.cs
+++ src/AccessTokenManagement/AccessTokenRequestSynchronization.cs
@@ -4,19 +4,30 @@ using System.Threading.Tasks;
 
 namespace IdentityModel.AspNetCore
 {
+    /// <summary>
+    /// Service to provide a concurrent dictionary for synchronizing token endpoint requests
+    /// </summary>
     public interface IAccessTokenRequestSynchronization
     {
+        /// <summary>
+        /// Concurrent dictionary as synchronization primitive
+        /// </summary>
         public ConcurrentDictionary<string, Lazy<Task<string>>> Dictionary { get; }
     }
 
+
+    /// <inheritdoc />
     public interface IClientAccessTokenRequestSynchronization : IAccessTokenRequestSynchronization
     { }
-    
+
+    /// <inheritdoc />
     public interface IUserAccessTokenRequestSynchronization : IAccessTokenRequestSynchronization
     { }
 
+    /// <inheritdoc />
     public class AccessTokenRequestSynchronization : IClientAccessTokenRequestSynchronization, IUserAccessTokenRequestSynchronization
     {
+        /// <inheritdoc />
         public ConcurrentDictionary<string, Lazy<Task<string>>> Dictionary { get; } = new();
     }
 }
