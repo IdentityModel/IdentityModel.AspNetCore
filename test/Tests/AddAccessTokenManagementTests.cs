8e201e632ee21858268edfc50f90da1c76e2e430
David Gardiner <david@gardiner.net.au>
2021-12-12 15:06:39 +1030

    Add overloads that include IServiceProvider in delegate
    - Implements #239

---
+++ test/Tests/AddAccessTokenManagementTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using IdentityModel.AspNetCore.AccessTokenManagement;
+using IdentityModel.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Tests
+{
+    public class AddAccessTokenManagementTests
+    {
+        public class CustomOptions
+        {
+            public string Thing { get; set; }
+        }
+
+        [Fact]
+        public void Using_overload_with_serviceprovider_should_succeed()
+        {
+            var services = new ServiceCollection();
+            var prefix = "Blah";
+            services.AddOptions<CustomOptions>()
+                .Configure(options =>
+                {
+                    options.Thing = prefix;
+                });
+
+            services.AddSingleton(provider => provider.GetRequiredService<IOptions<CustomOptions>>().Value);
+
+            services.AddAccessTokenManagement((provider, options) =>
+            {
+                var custom = provider.GetRequiredService<CustomOptions>();
+
+                options.Client.CacheKeyPrefix = custom.Thing;
+            });
+
+            var clientAccessTokenManagementOptions = services.BuildServiceProvider().GetRequiredService<ClientAccessTokenManagementOptions>();
+
+            Assert.Equal(prefix, clientAccessTokenManagementOptions.CacheKeyPrefix);
+        }
+    }
+}
