c79e3fd1a393cd8d53c9df04374dfbf1f242a7d0
Dominick Baier <dbaier@leastprivilege.com>
2022-01-15 13:13:48 +0100

    Cleanup

---
+++ samples/BlazorServer/HostingExtensions.cs
@@ -0,0 +1,103 @@
+using BlazorServer.Plumbing;
+using BlazorServer.Services;
+using IdentityModel.AspNetCore.AccessTokenManagement;
+using Serilog;
+
+namespace BlazorServer;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+                options.DefaultSignOutScheme = "oidc";
+            })
+            .AddCookie("cookie", options =>
+            {
+                options.Cookie.Name = "__Host-blazor";
+                options.Cookie.SameSite = SameSiteMode.Lax;
+            })
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://demo.duendesoftware.com";
+
+                // confidential client using code flow + PKCE
+                options.ClientId = "interactive.confidential.short";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+
+                options.MapInboundClaims = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+
+                // request scopes + refresh tokens
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("api");
+                options.Scope.Add("offline_access");
+
+                options.TokenValidationParameters.NameClaimType = "name";
+                options.TokenValidationParameters.RoleClaimType = "role";
+
+                options.Events.OnTokenValidated = async n =>
+                {
+                    var svc = n.HttpContext.RequestServices.GetRequiredService<IUserAccessTokenStore>();
+                    var exp = DateTimeOffset.UtcNow.AddSeconds(Double.Parse(n.TokenEndpointResponse!.ExpiresIn));
+
+                    await svc.StoreTokenAsync(n.Principal, n.TokenEndpointResponse.AccessToken, exp,
+                        n.TokenEndpointResponse.RefreshToken);
+                };
+            });
+
+        // adds access token management
+        builder.Services.AddAccessTokenManagement();
+
+        // not allowed to programmatically use HttpContext in Blazor Server.
+        // that's why tokens cannot be managed in the login session
+        builder.Services.AddSingleton<IUserAccessTokenStore, ServerSideTokenStore>();
+
+        // registers HTTP client that uses the managed user access token
+        builder.Services.AddUserAccessTokenHttpClient("client",
+            configureClient: client => { client.BaseAddress = new Uri("https://demo.duendesoftware.com/api/"); });
+
+        builder.Services.AddAuthorization(options =>
+        {
+            // By default, all incoming requests will be authorized according to the default policy
+            // comment out if you want to drive the login/logout workflow from the UI
+            options.FallbackPolicy = options.DefaultPolicy;
+        });
+
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+        builder.Services.AddSingleton<RemoteApiService>();
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+        
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapDefaultControllerRoute();
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+        
+        return app;
+    }
+}
--- samples/BlazorServer/Program.cs
+++ samples/BlazorServer/Program.cs
@@ -1,91 +1,38 @@
-using BlazorServer.Services;
-using BlazorServer.Plumbing;
-using IdentityModel.AspNetCore.AccessTokenManagement;
+using BlazorServer;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = "cookie";
-        options.DefaultChallengeScheme = "oidc";
-        options.DefaultSignOutScheme = "oidc";
-    })
-    .AddCookie("cookie", options =>
-    {
-        options.Cookie.Name = "__Host-blazor";
-        options.Cookie.SameSite = SameSiteMode.Lax;
-    })
-    .AddOpenIdConnect("oidc", options =>
-    {
-        options.Authority = "https://demo.duendesoftware.com";
-    
-        // confidential client using code flow + PKCE
-        options.ClientId = "interactive.confidential";
-        options.ClientSecret = "secret";
-        options.ResponseType = "code";
-        options.ResponseMode = "query";
-    
-        options.MapInboundClaims = false;
-        options.GetClaimsFromUserInfoEndpoint = true;
-        options.SaveTokens = true;
-    
-        // request scopes + refresh tokens
-        options.Scope.Clear();
-        options.Scope.Add("openid");
-        options.Scope.Add("profile");
-        options.Scope.Add("api");
-        options.Scope.Add("offline_access");
+Log.Information("Starting up");
 
-        options.TokenValidationParameters.NameClaimType = "name";
-        options.TokenValidationParameters.RoleClaimType = "role";
-        
-        options.Events.OnTokenValidated = async n => 
-        {
-            var svc = n.HttpContext.RequestServices.GetRequiredService<IUserAccessTokenStore>();
-            var exp = DateTimeOffset.UtcNow.AddSeconds(Double.Parse(n.TokenEndpointResponse!.ExpiresIn));
-            
-            await svc.StoreTokenAsync(n.Principal, n.TokenEndpointResponse.AccessToken, exp, n.TokenEndpointResponse.RefreshToken);
-        };
-    });
-
-// adds access token management
-builder.Services.AddAccessTokenManagement();
-
-// not allowed to programmatically use HttpContext in Blazor Server.
-// that's why tokens cannot be managed in the login session
-builder.Services.AddSingleton<IUserAccessTokenStore, ServerSideTokenStore>();
-
-// registers HTTP client that uses the managed user access token
-builder.Services.AddUserAccessTokenHttpClient("client", configureClient: client =>
+try
 {
-    client.BaseAddress = new Uri("https://demo.duendesoftware.com/api/");
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthorization(options =>
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("System", LogEventLevel.Error)
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+        .MinimumLevel.Override("System.Net.Http", LogEventLevel.Information)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext());
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+    
+    app.Run();
+}
+catch (Exception ex)
 {
-    // By default, all incoming requests will be authorized according to the default policy
-    // comment out if you want to drive the login/logout workflow from the UI
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-builder.Services.AddSingleton<RemoteApiService>();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapDefaultControllerRoute();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
